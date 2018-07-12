348718c26f2647a69e8ca50ba3c218ec7f08d0fd
vagrant <vagrant@drupalvm.test>
2018-07-12 15:49:55 +0000

    re-factored datasets

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 var domain = 'http://18.221.72.54:8000';
-var endpoint = '/api/transactions/aggregations/';
-var query = '?format=json&group_by=recipient_country&aggregations=activity_count,disbursement&reporting_organisation_identifier=XM-DAC-2-10&transaction_date_year=2016';
 var app = require('./webserver')();
 var request = require('request');
 
 // 1. List datasets
-app.get('/datasets', function(req, res) {
+app.get('/datasets', function (req, res) {
     if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
         return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
 
@@ -21,7 +19,7 @@ app.get('/datasets', function(req, res) {
             {id: 'name', name: {en: 'Country name'}, type: 'hierarchy'},
             {id: 'disbursement', name: {en: 'Country co-ordinates'}, type: 'numeric'},
             {id: 'latitude', name: {en: 'Latitude'}, type: 'numeric'},
-            {id: 'longitude', name: {en: 'Longitude'}, type: 'numeric'},
+            {id: 'longitude', name: {en: 'Longitude'}, type: 'numeric'}
         ]
     });
 
@@ -29,24 +27,27 @@ app.get('/datasets', function(req, res) {
 });
 
 // 2. Retrieve data slices
-app.post('/query', function(req, res) {
-  if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
-    return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
+app.post('/query', function (req, res) {
+    if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
+        return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
 
-  switch (req.body.id) {
-      case 'mapcountrytrans':
-      request.get({
-              uri: domain + endpoint + query,
-              gzip: true,
-              json: true
-          }, function (error, data) {
-              if (error)
-                  return res.status(500).end('Internal Server Error');
-              var datasets = data.body.results.map(function (result) {
-                  return [result.recipient_country.name, result.disbursement, result.recipient_country.location.coordinates[1], result.recipient_country.location.coordinates[0]];
-              });
-              return res.status(200).json(datasets);
-          });
-      break;
-  }
+    switch (req.body.id) {
+        case 'mapcountrytrans':
+            var endpoint = '/api/transactions/aggregations/';
+            var query = '?format=json&group_by=recipient_country&aggregations=activity_count,disbursement&reporting_organisation_identifier=XM-DAC-2-10&transaction_date_year=2016';
+
+            request.get({
+                uri: domain + endpoint + query,
+                gzip: true,
+                json: true
+            }, function (error, data) {
+                if (error)
+                    return res.status(500).end('Internal Server Error');
+                var datasets = data.body.results.map(function (result) {
+                    return [result.recipient_country.name, result.disbursement, result.recipient_country.location.coordinates[1], result.recipient_country.location.coordinates[0]];
+                });
+                return res.status(200).json(datasets);
+            });
+            break;
+    }
 });
