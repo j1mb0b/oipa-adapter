e47c817c6b66099ea8fa31a20c8fe6e2bbaee3a0
vagrant <vagrant@drupalvm.test>
2018-07-12 15:35:55 +0000

    re-factored datasets

--- index.js
+++ index.js
@@ -40,18 +40,20 @@ app.post('/query', function(req, res) {
   if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
     return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
 
-  console.log(req);
-
-  request.get({
-    uri: domain + endpoint + query,
-    gzip: true,
-    json: true
-  }, function(error, data) {
-    if (error)
-      return res.status(500).end('Internal Server Error');
-    var datasets = data.body.results.map(function(result) {
-      return [result.recipient_country.name, result.disbursement, result.recipient_country.location.coordinates[1], result.recipient_country.location.coordinates[0]];
-    });
-    return res.status(200).json(datasets);
-  });
+  switch (req.body.id) {
+      case 'mapcountrytrans':
+      request.get({
+              uri: domain + endpoint + query,
+              gzip: true,
+              json: true
+          }, function (error, data) {
+              if (error)
+                  return res.status(500).end('Internal Server Error');
+              var datasets = data.body.results.map(function (result) {
+                  return [result.recipient_country.name, result.disbursement, result.recipient_country.location.coordinates[1], result.recipient_country.location.coordinates[0]];
+              });
+              return res.status(200).json(datasets);
+          });
+      break;
+  }
 });
