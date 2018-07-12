7ffed47c2cef9bd3d5ce49a3aee9ee5604c21f94
vagrant <vagrant@drupalvm.test>
2018-07-12 15:26:57 +0000

    re-factored datasets

--- index.js
+++ index.js
@@ -7,60 +7,49 @@ var app = require('./webserver')();
 var request = require('request');
 
 // 1. List datasets
-app.get('/datasets', function (req, res) {
-    if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
-        return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
+app.get('/datasets', function(req, res) {
+  if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
+    return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
 
-    request.get({
-        uri: domain + endpoint + query + "&page=1&page_size=1",
-        gzip: true,
-        json: true
-    }, function (error, data) {
-        if (error)
-            return res.status(500).end('Internal Server Error');
-        var datasets = data.body.results.map(function (result) {
-            return {
-                id: 'mapcountrytrans',
-                name: {en: 'Map country transactions data'},
-                description: {en: 'Country map placement for transaction data'},
-                columns: [
-                    {id: 'name', name: {en: 'Country name'}, type: 'hierarchy'},
-                    {id: 'disbursement', name: {en: 'Country co-ordinates'}, type: 'numeric'},
-                    {id: 'latitude', name: {en: 'Latitude'}, type: 'numeric'},
-                    {id: 'longitude', name: {en: 'Longitude'}, type: 'numeric'},
-                ]
-            }
-        });
-        return res.status(200).json(datasets);
+  request.get({
+    uri: domain + endpoint + query + "&page=1&page_size=1",
+    gzip: true,
+    json: true
+  }, function(error, data) {
+    if (error)
+      return res.status(500).end('Internal Server Error');
+    var datasets = data.body.results.map(function(result) {
+      return {
+        id: 'mapcountrytrans',
+        name: {en: 'Map country transactions data'},
+        description: {en: 'Country map placement for transaction data'},
+        columns: [
+          {id: 'name', name: {en: 'Country name'}, type: 'hierarchy'},
+          {id: 'disbursement', name: {en: 'Country co-ordinates'}, type: 'numeric'},
+          {id: 'latitude', name: {en: 'Latitude'}, type: 'numeric'},
+          {id: 'longitude', name: {en: 'Longitude'}, type: 'numeric'},
+        ]
+      }
     });
+    return res.status(200).json(datasets);
+  });
 });
 
 // 2. Retrieve data slices
-app.post('/query', function (req, res) {
-    if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
-        return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
+app.post('/query', function(req, res) {
+  if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
+    return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
 
-    // Set request specific properties for a given data set.
-    var dataset_uri = false;
-    var dataset_output = false;
-    switch (req.body.id) {
-        default:
-            dataset_uri = domain + endpoint + query;
-            dataset_output = [result.recipient_country.name, result.disbursement, result.recipient_country.location.coordinates[1], result.recipient_country.location.coordinates[0]];
-            break;
-    }
-
-    // Preform generic request.
-    request.get({
-        uri: dataset_uri,
-        gzip: true,
-        json: true
-    }, function (error, data) {
-        if (error)
-            return res.status(500).end('Internal Server Error');
-        var datasets = data.body.results.map(function (result) {
-            return dataset_output;
-        });
-        return res.status(200).json(datasets);
+  request.get({
+    uri: domain + endpoint + query,
+    gzip: true,
+    json: true
+  }, function(error, data) {
+    if (error)
+      return res.status(500).end('Internal Server Error');
+    var datasets = data.body.results.map(function(result) {
+      return [result.recipient_country.name, result.disbursement, result.recipient_country.location.coordinates[1], result.recipient_country.location.coordinates[0]];
     });
+    return res.status(200).json(datasets);
+  });
 });
