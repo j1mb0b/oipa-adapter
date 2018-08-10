438ab72201a28954a193ec1fb122c5abd86c27a0
vagrant <vagrant@drupalvm.test>
2018-08-10 13:03:14 +0000

    latest

--- functions.js
+++ functions.js
@@ -9,31 +9,7 @@ module.exports = class Api {
         this.domain = domain;
     }
 
-    getProjects(url) {
-        request.get({
-            uri: url,
-            gzip: true,
-            json: true
-        }, function (error, data) {
-            if (error)
-                return this.res.status(500).end('Internal Server Error');
-
-            datasets.push(data.body.results.map(function (result) {
-                let locations = [];
-                this.getLocations(this.domain + "/api/locations/?format=json&activity_id=XM-DAC-2-10", locations);
-                return [result.iati_identifier, locations];
-            }));
-
-            if (result.next) {
-                self.getProjects(result.next);
-            }
-            else {
-                return this.res.status(200).json(datasets);
-            }
-        });
-    }
-
-    getLocations(url, locations) {
+    async getLocations(url, locations) {
         request.get({
             uri: url,
             gzip: true,
@@ -48,6 +24,30 @@ module.exports = class Api {
 
             if (result.next) {
                 self.getLocations(result.next, locations);
+            }
+        });
+    }
+
+    getProjects(url) {
+        request.get({
+            uri: url,
+            gzip: true,
+            json: true
+        }, function (error, data) {
+            if (error)
+                return this.res.status(500).end('Internal Server Error');
+
+            datasets.push(data.body.results.map(function (result) {
+                let locations = [];
+                this.getLocations(this.domain + "/api/locations/?format=json&activity_id=" + result.iati_identifier, locations);
+                return [result.iati_identifier, locations];
+            }));
+
+            if (result.next) {
+                self.getProjects(result.next);
+            }
+            else {
+                return this.res.status(200).json(datasets);
             }
         });
     }
