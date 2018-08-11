284a97a7032c269d81b8aa16940e8abc5fad7c38
vagrant <vagrant@drupalvm.test>
2018-08-11 17:07:49 +0000

    latest

--- functions.js
+++ functions.js
@@ -20,7 +20,7 @@ module.exports = {
         });
     },
     activity: async function (url, domain, type) {
-        request({
+        await request({
             "method": "GET",
             "uri": url,
             "json": true
