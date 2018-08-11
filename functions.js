b3925e258055e6c9bab17bf38b17b22e93446552
vagrant <vagrant@drupalvm.test>
2018-08-11 16:45:29 +0000

    latest

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@
 const NodeCache = require("node-cache");
 const oipaCache = new NodeCache();
 let request = require('request-promise');
+let output = [];
 module.exports = {
     cacheGet: function (key) {
         oipaCache.get(key, function (err, value) {
@@ -18,36 +19,28 @@ module.exports = {
             }
         });
     },
-    activity: async function (url) {
-        let output = ['pds'];
-        await function getActivity(url, type) {
-            try {
-                request({
-                    "method": "GET",
-                    "uri": url,
-                    "json": true
-                }).then(function (data) {
-                    if (type === "location") {
-                        data.locations.map(function (loc) {
-                            if (loc.point.pos !== null && Object.keys(loc.point.pos).length > 0)
-                                output.push(loc.point.pos.latitude, loc.point.pos.longitude);
-                        });
-                    }
-                    else {
-                        data.results.map(function (result) {
-                            getActivity(result.url, "location");
-                        });
-                    }
-
-                    if (data.next)
-                        getActivity(data.next, "activity");
+    activity: function (url, domain, type) {
+        output.push(request({
+            "method": "GET",
+            "uri": url,
+            "json": true
+        }).then(function (data) {
+            if (type === "location") {
+                data.locations.map(function (loc) {
+                    if (loc.point.pos !== null && Object.keys(loc.point.pos).length > 0)
+                        return [loc.point.pos.latitude, loc.point.pos.longitude];
                 });
             }
-            catch (err) {
-                console.log(err);
+            else {
+                data.results.map(function (result) {
+                    module.exports.activity(result.url, domain, "location");
+                });
             }
-        };
-        console.log(output);
+
+            if (data.next)
+                module.exports.activity(data.next, domain, "activity");
+        }));
+
         return output;
     }
 };
