7823facade0f57d07d44543dfcd97b5a6148562d
vagrant <vagrant@drupalvm.test>
2018-08-11 15:09:21 +0000

    latest

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post('/query', function (req, res) {
             endpoint = '/api/activities/';
             tools.activity(domain + endpoint + default_params, domain, "activity")
                 .then(response => {
-                    console.log(JSON.parse(response.body).data);
+                    console.log(JSON.parse(response));
                     return res.status(200).json(response);
                 })
                 .catch(error => console.log(error));
