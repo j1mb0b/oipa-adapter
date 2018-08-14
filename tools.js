266472f688e15ead3839af3599a9b5ef0128d2fe
vagrant <vagrant@drupalvm.test>
2018-08-14 13:56:54 +0000

    lastet

--- tools.js
+++ tools.js
@@ -85,7 +85,7 @@ module.exports = {
     main: function(endpoint) {
         const CACHE_DURATION = 600;
         const CACHE_KEY = endpoint;
-        cacheProvider.instance().get(CACHE_KEY, function(err, value) {
+        return cacheProvider.instance().get(CACHE_KEY, function(err, value) {
             if (err) console.error(err);
             if (value === undefined) {
                 console.log('setting cache...');
@@ -94,13 +94,13 @@ module.exports = {
 
                 cacheProvider.instance().set(CACHE_KEY, res, CACHE_DURATION, function(err, success) {
                     if (!err && success) {
-                        endpoint(res);
+                        return res;
                     }
                 });
             }
             else {
                 console.log('worked!');
-                endpoint(value);
+                return value;
             }
         });
     }
