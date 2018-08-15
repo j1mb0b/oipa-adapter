e21edf7eea79a06ffb9c09f649982b6e60ab3770
vagrant <vagrant@drupalvm.test>
2018-08-15 11:39:25 +0000

    lastet

--- tools.js
+++ tools.js
@@ -89,9 +89,9 @@ module.exports = {
         let poly = items;
         poly[0].countries = [];
 
-        return Promise.map(items[0].countries, function (item) {
+        return Promise.map(items[0].countries, async function (item) {
             console.log(item);
-            return request(module.exports.getOptions(item)).then(response => {
+            return await request(module.exports.getOptions(item)).then(response => {
                 console.log(response);
                 poly[0].countries.push(response.polygon);
                 return poly;
