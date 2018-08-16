20d61e4b05ab86522bdab3620e64ee2cef372973
vagrant <vagrant@drupalvm.test>
2018-08-16 06:41:48 +0000

    lastet

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -6,11 +6,17 @@ const map = L.map('map', {
     scrollWheelZoom: false
 });
 
+L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
 // Set the position and zoom level of the map
 map.setView([47.70, 13.35], 7);
 
 // Initialize the base layer
-const osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const osm_mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
