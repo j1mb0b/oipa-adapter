3ee8ac9fc9c7c0633167fc8dc6c882fe36e7601f
vagrant <vagrant@drupalvm.test>
2018-08-16 06:45:47 +0000

    lastet

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -3,7 +3,7 @@ const L = require('leaflet');
 
 // Initialize the map
 const map = L.map('map', {
-    scrollWheelZoom: false
+    scrollWheelZoom: true
 });
 
 L.polygon([
@@ -16,7 +16,9 @@ L.polygon([
 map.setView([47.70, 13.35], 7);
 
 // Initialize the base layer
-const osm_mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 0,
+    id: 'map',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
