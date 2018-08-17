029928b7df9dbf4ef467f2dbade88009b0e27fbc
vagrant <vagrant@drupalvm.test>
2018-08-17 11:32:33 +0000

    lastet

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -1,16 +1,15 @@
 // Initialize leaflet.js
 const L = require('leaflet');
-let request = require('request-promise');
 let jsdom = require('jsdom');
-const {JSDOM} = jsdom;
-const {window} = new JSDOM();
-const {document} = (new JSDOM('')).window;
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
 global.document = document;
 
 let $ = jQuery = require('jquery');
 
-$(document).ready(function () {
-    (function (global, undefined) {
+$(document).ready(function() {
+    (function(global, undefined){
         //The following method is created based on Ray casting algorithm
         //Source: https://github.com/substack/point-in-polygon
         //Source: https://wrf.ecse.rpi.edu//Research/Short_Notes/pnpoly.html
@@ -18,22 +17,21 @@ $(document).ready(function () {
             var polyPoints = poly.getLatLngs();
             var x = marker.getLatLng().lat, y = marker.getLatLng().lng;
             var inside = false;
-            for (var a = 0; a < polyPoints.length; a++) {
+            for(var a = 0; a < polyPoints.length; a++){
                 for (var i = 0, j = polyPoints[a].length - 1; i < polyPoints[a].length; j = i++) {
                     var xi = polyPoints[a][i].lat, yi = polyPoints[a][i].lng;
                     var xj = polyPoints[a][j].lat, yj = polyPoints[a][j].lng;
                     var intersect = ((yi > y) != (yj > y))
                         && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-                    if (intersect) {
+                    if (intersect){
                         inside = !inside;
                     }
                 }
             }
             return inside;
         };
-
         //helpers for the markers
-        function markerOptions(id, title) {
+        function markerOptions(id,title) {
             var geojsonMarkerOptionsStuff = {
                 radius: 6,
                 fillColor: "#a04567",
@@ -41,8 +39,8 @@ $(document).ready(function () {
                 weight: 1,
                 opacity: 1,
                 fillOpacity: 0.8,
-                id: id,
-                title: title
+                id:id,
+                title:title
             };
             return geojsonMarkerOptionsStuff
         }
@@ -50,12 +48,12 @@ $(document).ready(function () {
         function buildClusterPopupHtml(locations) {
             //alert(iatiTitle + iatiId);
             var items = [];
-            for (var i = 0; i < locations.length; i++) {
+            for(var i = 0; i < locations.length; i++) {
                 var location = locations[i];
                 items.push([
                     "<div class='row'>",
                     "<div class='five columns location-label'>",
-                    "<a href='/projects/", location.id, "'>", location.id, "</a>",
+                    "<a href='/projects/", location.id ,"'>", location.id, "</a>",
                     "</div>",
                     "<div class='seven columns'>", location.title, "</div>",
                     "</div>"
@@ -100,13 +98,13 @@ $(document).ready(function () {
         if (projectType == "global") {
 
             map = new L.Map('countryMap', {
-                center: new L.LatLng(7.79, 21.28),
+                center: new L.LatLng(7.79,21.28),
                 zoom: 1,
                 layers: [mapBox]
             });
         } else if (countryName && countryCode) {
 
-            if (countryBounds[countryCode][2] != null) {
+            if (countryBounds[countryCode][2] != null){
                 zoomFactor = countryBounds[countryCode][2];
                 console.log(zoomFactor);
             }
@@ -139,43 +137,34 @@ $(document).ready(function () {
 
         //Draw a polygon on the map to bound the exact country position
         var multiVertices = new Array();
-        for (var countryPolygonesDefArrayIndex = 0; countryPolygonesDefArrayIndex < polygonsData[countryCode].length; countryPolygonesDefArrayIndex++) {
+        for (var countryPolygonesDefArrayIndex=0; countryPolygonesDefArrayIndex<polygonsData[countryCode].length; countryPolygonesDefArrayIndex++) {
             var countryPolygoneDefString = polygonsData[countryCode][countryPolygonesDefArrayIndex];
             var verticesDefArray = countryPolygoneDefString.split(" ");
             var vertices = new Array();
-            for (var vertexDefStringIndex = 0; vertexDefStringIndex < verticesDefArray.length; vertexDefStringIndex++) {
+            for (var vertexDefStringIndex=0; vertexDefStringIndex<verticesDefArray.length; vertexDefStringIndex++) {
                 var vertexDefString = verticesDefArray[vertexDefStringIndex].split(",");
-                var longitude = vertexDefString[0];
-                var latitude = vertexDefString[1];
-                var latLng = new L.LatLng(latitude, longitude);
-                vertices[vertices.length] = latLng;
+                var longitude=vertexDefString[0];
+                var latitude=vertexDefString[1];
+                var latLng=new L.LatLng(latitude,longitude);
+                vertices[vertices.length]=latLng;
             }
-            multiVertices[multiVertices.length] = vertices;
+            multiVertices[multiVertices.length]=vertices;
         }
         console.log(multiVertices);
-        var multiPolygon = L.polygon(multiVertices, {
+        var multiPolygon = L.polygon(multiVertices,{
             stroke: true, /* draws the border when true */
             color: 'red', /* border color */
             weight: 1, /* stroke width in pixels */
-            fill: true,
+            fill:true,
             fillColor: '#204B63',//"#204B63",
             fillOpacity: 0.4
         }).addTo(map);
 
         // create the geopoints if any are defined
-        if (map) {
+        if(map) {
             //alert("start processing datapoints");
-            var url = "http://18.221.72.54:8000/api/activities/?format=json&reporting_organisation=" + reportingOrgs + "&hierarchy=1&recipient_country=&fields=title,iati_identifier,locations&page_size=500&activity_status=2";
-            request({
-                method: 'GET',
-                url: url,
-                json: true,
-                headers: {
-                    'Connection': 'keep-alive',
-                    'Accept-Encoding': '',
-                    'Accept-Language': 'en-US,en;q=0.8'
-                }
-            }).then(function (iati) {
+            var url = "http://18.221.72.54:8000/api/activities/?format=json&reporting_organisation="+reportingOrgs+"&hierarchy=1&recipient_country=&fields=title,iati_identifier,locations&page_size=500&activity_status=2";
+            $.getJSON("activity.json", function (iati) {
                 $('.modal_map_markers').show();
                 //set up markerCluster
                 var markers = new L.MarkerClusterGroup({
@@ -184,24 +173,24 @@ $(document).ready(function () {
                     singleMarkerMode: true,
                     maxClusterRadius: 40,
                     removeOutsideVisibleBounds: false,
-                    iconCreateFunction: function (cluster) {
+                    iconCreateFunction: function(cluster) {
                         var count = cluster.getChildCount();
                         var additional = ""
-                        if (count > 99) {
+                        if(count > 99) {
                             count = "+";
                             additional = "large-value";
                         }
 
-                        return new L.DivIcon({html: '<div class="marker cluster ' + additional + '">' + count + '</div>'});
+                        return new L.DivIcon({ html: '<div class="marker cluster ' + additional + '">' + count+ '</div>' });
                     }
                 });
 
                 markers.on('clusterclick', function (a) {
                     //alert("clusterclick" +  a.target._zoom + " "  + a.target._maxZoom);
                     var atMax = a.target._zoom == a.target._maxZoom
-                    if (atMax) {
+                    if(atMax) {
                         var clusterLocations = [];
-                        for (var i = 0; i < a.layer._markers.length; i++) {
+                        for(var i = 0; i < a.layer._markers.length; i++) {
                             clusterLocations.push(a.layer._markers[i].options);
                         }
 
@@ -221,20 +210,20 @@ $(document).ready(function () {
                     var title = (d.title.narratives != null) ? d.title.narratives[0].text : "";
                     //iterate over each location
                     d.locations.forEach(function (p) {
-                        try {
-                            var latlng = L.latLng(p.point.pos.latitude, p.point.pos.longitude);
-                            var marker = new L.circleMarker(latlng, markerOptions(iatiIdentifier, title));
+                        try{
+                            var latlng = L.latLng(p.point.pos.latitude,p.point.pos.longitude);
+                            var marker = new L.circleMarker(latlng, markerOptions(iatiIdentifier,title));
                             //create popup text
                             var locationName = p.name[0].narratives[0].text;
                             marker.bindPopup("<a href='" + dtUrl + "'>" + title + " (" + iatiIdentifier + ")</a>" + "<br />" + locationName);
                             //if(tempBreaker == 0 && (p.administrative[0].code == countryCode || p.name[0].narratives[0].text)){
                             //if(tempBreaker == 0 && (p.name[0].narratives[0].text.includes(countryName) || p.description[0].narratives[0].text.includes(countryName))){
-                            if (isMarkerInsidePolygon(marker, multiPolygon)) {
+                            if(isMarkerInsidePolygon(marker,multiPolygon)){
                                 //add to the map layer
                                 markers.addLayer(marker);
                             }
                         }
-                        catch (e) {
+                        catch(e){
                             console.log(iatiIdentifier);
                             console.log("variable doesn't exist");
                         }
@@ -242,16 +231,8 @@ $(document).ready(function () {
                 });
 
                 map.addLayer(markers);
-            }).finally(function () {
+            }).done(function(){
                 $('.modal_map_markers').hide();
-            }).catch(function (err) {
-                if (err.message === 'read ECONNRESET') {
-                    console.log('Timed out :(');
-                    return false;
-                } else {
-                    console.log('Error.');
-                    throw err;
-                }
             });
         }
 
