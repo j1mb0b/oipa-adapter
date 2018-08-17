626ffc2b44ff0f79e041dcacfa31585f1a215ca6
vagrant <vagrant@drupalvm.test>
2018-08-17 11:19:18 +0000

    lastet

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -163,7 +163,7 @@ $(document).ready(function() {
         // create the geopoints if any are defined
         if(map) {
             //alert("start processing datapoints");
-            var url = "http://18.221.72.54:8000/api/activities/?format=json&reporting_organisation="+document.reportingOrgs+"&hierarchy=1&recipient_country=&fields=title,iati_identifier,locations&page_size=500&activity_status=2";
+            var url = "http://18.221.72.54:8000/api/activities/?format=json&reporting_organisation="+reportingOrgs+"&hierarchy=1&recipient_country=&fields=title,iati_identifier,locations&page_size=500&activity_status=2";
             $.getJSON(url, function (iati) {
                 $('.modal_map_markers').show();
                 //set up markerCluster
