a0757bd6d16fe0f9213df51caa699c09ebc58724
vagrant <vagrant@drupalvm.test>
2018-08-17 11:35:08 +0000

    lastet

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -164,7 +164,7 @@ $(document).ready(function() {
         if(map) {
             //alert("start processing datapoints");
             var url = "http://18.221.72.54:8000/api/activities/?format=json&reporting_organisation="+reportingOrgs+"&hierarchy=1&recipient_country=&fields=title,iati_identifier,locations&page_size=500&activity_status=2";
-            $.getJSON("activity.json", function (iati) {
+            $.getJSON("/scripts/leaflet/activity.json", function (iati) {
                 $('.modal_map_markers').show();
                 //set up markerCluster
                 var markers = new L.MarkerClusterGroup({
