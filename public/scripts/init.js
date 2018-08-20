7faf8305415ca7a4f81804c5bbd092dba6073c34
vagrant <vagrant@drupalvm.test>
2018-08-20 08:39:49 +0000

    lastet

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -89,7 +89,7 @@ $(document).ready(function () {
             }
         });
 
-        var cartoDB = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+        var cartoDB = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
         });
 
