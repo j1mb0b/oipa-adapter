ba787b1b4334dfbf3e6ec44dde329ee9cf8ce241
vagrant <vagrant@drupalvm.test>
2018-08-24 10:50:06 +0000

    updated.

--- index.js
+++ index.js
@@ -48,9 +48,6 @@ app.get('/getCountryData', function (req, res) {
     if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
         return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
 
-    if (!req.body.id)
-        return res.status(403).end('Please set "id" in the body of your request!');
-
     // Default variables.
     let url = domain + '/api/activities/?format=json&reporting_organisation_identifier=XM-DAC-2-10';
     return cacheProvider.instance().get(url, function (err, value) {
@@ -69,38 +66,57 @@ app.get('/getCountryData', function (req, res) {
     });
 });
 
+// 2. Retrieve data slices
 app.post('/query', function (req, res) {
-    let endpoint = '/api/transactions/aggregations/';
-    let groupOrderBy = 'transaction_date_year';
-    let country_code = "MA";
-    // Get the key used for logic, filtering and getting a property from the response.
-    let aggr_type = req.body.id.match(/country-(.*)/)[1];
-    // Handle "country-value" since it uses a different endpoint, group, and order by.
-    if (aggr_type === 'value') {
-        endpoint = '/api/budgets/aggregations/';
-        // Exception for Budget since it uses a different field to the others.
-        groupOrderBy = 'budget_period_end_year';
-    }
-    // Build query string.
-    let query = default_params + '&group_by=' + groupOrderBy + '&aggregations=' + aggr_type + '&order_by=' + groupOrderBy + '&recipient_country=' + country_code;
-    let uri = domain + endpoint + query;
-    request.get({
-        uri: uri,
-        gzip: true,
-        json: true
-    }, function (error, data) {
-        if (error || !data.body.results) {
-            console.log(uri);
-            return res.status(500).end('Internal Server Error');
-        }
-        let datasets = data.body.results.map(function (result) {
-            let obj = Object.keys(result);
-            // We assume the order of keys are first: transaction year, second: amount.
-            // Also that it remains the same for the other "cases", if not we are forced to
-            // hard code the string to get the value which won't work well with this generic code.
-            return [result[obj[0]], result[obj[1]]];
-        });
-        return res.status(200).json(datasets);
-    });
+    if (req.headers['x-secret'] !== process.env.CUMULIO_SECRET)
+        return res.status(403).end('Given plugin secret does not match Cumul.io plugin secret.');
 
+    if (!req.body.id)
+        return res.status(403).end('Please set "id" in the body of your request!');
+
+    // Default variables.
+    let default_params = '?format=json&reporting_organisation_identifier=XM-DAC-2-10';
+    let endpoint = "";
+    let query = "";
+    switch (req.body.id) {
+        case 'country-disbursement':
+        case 'country-commitment':
+        case 'country-value':
+            endpoint = '/api/transactions/aggregations/';
+            let groupOrderBy = 'transaction_date_year';
+            let country_code = (req.body.country_code) ? req.body.country_code : "MA";
+            // Get the key used for logic, filtering and getting a property from the response.
+            let aggr_type = req.body.id.match(/country-(.*)/)[1];
+            // Handle "country-value" since it uses a different endpoint, group, and order by.
+            if (aggr_type === 'value') {
+                endpoint = '/api/budgets/aggregations/';
+                // Exception for Budget since it uses a different field to the others.
+                groupOrderBy = 'budget_period_end_year';
+            }
+            // Build query string.
+            query = default_params + '&group_by=' + groupOrderBy + '&aggregations=' + aggr_type + '&order_by=' + groupOrderBy + '&recipient_country=' + country_code;
+            let uri = domain + endpoint + query;
+            request.get({
+                uri: uri,
+                gzip: true,
+                json: true
+            }, function (error, data) {
+                if (error || !data.body.results) {
+                    console.log(uri);
+                    return res.status(500).end('Internal Server Error');
+                }
+                let datasets = data.body.results.map(function (result) {
+                    let obj = Object.keys(result);
+                    // We assume the order of keys are first: transaction year, second: amount.
+                    // Also that it remains the same for the other "cases", if not we are forced to
+                    // hard code the string to get the value which won't work well with this generic code.
+                    return [result[obj[0]], result[obj[1]]];
+                });
+                return res.status(200).json(datasets);
+            });
+            break;
+
+        default:
+            return res.status(403).end('No match for data set ID: ' + req.body.id);
+    }
 });
