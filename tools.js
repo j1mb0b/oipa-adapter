58bdf09db35320c3850fb43fbc6b6c8225449b0d
vagrant <vagrant@drupalvm.test>
2018-09-10 15:47:06 +0000

    updated.

--- tools.js
+++ tools.js
@@ -26,7 +26,7 @@ module.exports = {
                 switch (type) {
                     case "sectors":
                         return Promise.map(data.results, function (result) {
-                            return Promise.all(["parent":module.exports.query(result.sector.url, "")]);
+                            return Promise.all([module.exports.query(result.sector.url, "")]);
                         }, { concurrency: 5}).then(function(data) {
                             return data[0];
                         });
