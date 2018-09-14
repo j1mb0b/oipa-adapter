bbc77342cbd0523e9c4c76731dbbe1a20364bf9b
vagrant <vagrant@drupalvm.test>
2018-09-14 07:51:54 +0000

    updated.

--- tools.js
+++ tools.js
@@ -33,7 +33,7 @@ module.exports = {
 
                     case "documents":
                         data.results.map(function (docs){
-                            data.doucment_links[docs.document_links.id] = docs.document_links;
+                            data[docs.document_links.id] = docs.document_links;
                         });
                         delete data.results;
                         return data;
