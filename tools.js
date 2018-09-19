30a2d7808c81387e5e43a47c4c6a846866be9c52
vagrant <vagrant@drupalvm.test>
2018-09-19 16:06:59 +0000

    Updated version

--- tools.js
+++ tools.js
@@ -45,9 +45,10 @@ module.exports = {
 
                 case "countries":
                     let cc = {};
-                    if (output["results"] === undefined && output["country_data"] === undefined)
+                    if (!output["results"] && !output["country_data"]) {
                         output["results"] = [];
                         output["country_data"] = [];
+                    }
                     data.results.map(function (result) {
                         // Get the countries at activity level and build a array.
                         // This is used to determine the polygon for valid locations.
