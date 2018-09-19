e0a1a1461ec1199d30ca3b0eec975ea94702d64f
vagrant <vagrant@drupalvm.test>
2018-09-19 15:43:47 +0000

    Updated version

--- tools.js
+++ tools.js
@@ -53,7 +53,7 @@ module.exports = {
                         // This is used to determine the polygon for valid locations.
                         if (result.recipient_countries.length > 0) {
                             result.recipient_countries.map(function (country) {
-                                if (!cc.hasOwnProperty('country.country.code')) {
+                                if (cc[country.country.code] === undefined) {
                                     cc[country.country.code] = {
                                         "country": country.country.name,
                                         "id": country.country.code,
